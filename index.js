f06f80f5ea33fdcb37143c3fb45ebbc6ba8a13cf
Suganya <suganya.p@angleritech.com>
2024-01-02 22:16:24 +0530

    "api llink setup completed"

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 const schemaData = mongoose.Schema({
     name: String,
     email:String,
-    mobile:Number
+    mobile:String
 },
 {
     timestamps : true
@@ -21,26 +21,59 @@ const schemaData = mongoose.Schema({
 const userModel = mongoose.model("user",schemaData)
 
 //read
+//http://localhost:5000/
 app.get("/", async(req,res)=>{
     const data = await userModel.find({})
     res.json({success : true, data : data})
 })
 
 //create data // save data in mongodb
+//http://localhost:5000/create
+/*
+{
+    name,
+    email,
+    mobile
+}
+*/
 app.post("/create", async(req,res)=>{
     console.log(req.body)
     const data = new userModel(req.body)
     await data.save()
-    res.send({success : true, message: "data save successfull"})
+    res.send({success : true, message: "data save successfully", data : data})
 })
 
 //Update data // save data in mongodb
+//http://localhost:5000/update
+/*
+{
+    id: "",
+    name : "",
+    email : "",
+    mobile : ""
+
+}
+*/
 app.put("/update", async(req,res)=>{
     console.log(req.body)
-    await userModel.updateOne({_id : req.body.id},{name : "sugan" })
+    const {id,...rest} = req.body
+    console.log(rest)
+    const data = await userModel.updateOne({_id : id},rest)
+   // await userModel.updateOne({_id : req.body.id},{name : "sugan" })
    
-    res.send({success : true, message: "data Update successfull"})
+    res.send({success : true, message: "data Update successfully", data : data})
 })
+
+//Delete data
+//http://localhost:5000/delete/658eb2433e8ce522b036249a
+app.delete("/delete/:id", async(req,res)=>{
+   
+    const id = req.params.id
+    console.log(id)
+    const data  = await userModel.deleteOne({_id : id})
+    res.send({success : true, message: "data Deleted successfully", data : data})
+})
+
 
 
 mongoose.connect("mongodb://localhost:27017/crudopertion")
